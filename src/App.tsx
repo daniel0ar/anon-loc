b063766cd5b9eb509004704b5cea65a3218e73fe
Daniel Arroyo <daniel0ar@hotmail.com>
2025-05-08 20:31:41 -0500

    refactor: Add Navbar logo

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,14 @@ function App() {
           <nav className="bg-white dark:bg-gray-900 sticky w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
               <a
-                href="https://flowbite.com/"
+                href="/"
                 className="flex items-center space-x-3 rtl:space-x-reverse"
               >
                 <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  className="h-8"
-                  alt="Flowbite Logo"
+                  src="/src/assets/logo.svg"
+                  className="h-6"
+                  alt="GPR Logo"
                 />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  Flowbite
-                </span>
               </a>
               <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button
@@ -69,7 +66,7 @@ function App() {
                   <li>
                     <Link
                       to="/"
-                      className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                      className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:p-0"
                       aria-current="page"
                     >
                       Home
@@ -78,7 +75,7 @@ function App() {
                   <li>
                     <Link
                       to="/gps"
-                      className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:text-blue-700 md:dark:text-blue-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     >
                       GPS Data
                     </Link>
