4131c1881e655dfc6e937643616b18be947b5919
Daniel Arroyo <daniel0ar@hotmail.com>
2025-05-08 20:54:47 -0500

    fix: Navbar menu not opening on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import GpsPage from "./components/GpsPage";
 import AdminPage from "./components/AdminPage";
 import { StarknetConfig, publicProvider, braavos } from "@starknet-react/core";
 import { mainnet, sepolia } from "@starknet-react/chains";
+import { useState } from "react";
 
 function App() {
   const connectors = [braavos()];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  
   return (
     <StarknetConfig
       chains={[mainnet, sepolia]}
@@ -34,11 +41,11 @@ function App() {
                   Get started
                 </button>
                 <button
-                  data-collapse-toggle="navbar-sticky"
+                  onClick={toggleMenu}
                   type="button"
                   className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                   aria-controls="navbar-sticky"
-                  aria-expanded="false"
+                  aria-expanded={isMenuOpen}
                 >
                   <span className="sr-only">Open main menu</span>
                   <svg
@@ -59,7 +66,7 @@ function App() {
                 </button>
               </div>
               <div
-                className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                className={`items-center justify-between ${isMenuOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`}
                 id="navbar-sticky"
               >
                 <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
