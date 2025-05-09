16a666d5d0488fa888482fc60d3b3f0bcdf9f23a
Daniel Arroyo <daniel0ar@hotmail.com>
2025-05-08 20:09:38 -0500

    chore: Add Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import basicSsl from '@vitejs/plugin-basic-ssl'
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
